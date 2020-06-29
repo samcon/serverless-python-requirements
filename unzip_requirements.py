c75bf6d66a3d574c22a6bae534a2e9a2cc4dbf8a
Samcon <samcon@users.noreply.github.com>
2020-06-29 09:54:57 +0300

    Not using a zipped layer anymore

--- unzip_requirements.py
+++ unzip_requirements.py
@@ -16,11 +16,6 @@ if not os.path.exists(pkgdir):
     default_lambda_task_root = os.environ.get('LAMBDA_TASK_ROOT', os.getcwd())
     lambda_task_root = os.getcwd() if os.environ.get('IS_LOCAL') == 'true' else default_lambda_task_root
     zip_requirements = os.path.join(lambda_task_root, '.requirements.zip')
-
     zipfile.ZipFile(zip_requirements, 'r').extractall(tempdir)
-
-    zip_requirements_layer_reqs = os.path.join('/opt/', '.requirements.zip')
-    if os.path.isfile(zip_requirements_layer_reqs):
-        zipfile.ZipFile(zip_requirements_layer_reqs, 'r').extractall(tempdir)
 
     os.rename(tempdir, pkgdir)  # Atomic
